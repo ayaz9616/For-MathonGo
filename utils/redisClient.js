71dd86eaeb9dbb424946ef217cecc5143714325c
ayaz9616 <ayaz9616835404@gmail.com>
2025-06-04 08:46:25 +0530

    improved redisClient

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,3 +1,3 @@
 const Redis = require('ioredis');
-const redisClient = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+const redisClient = new Redis(process.env.REDIS_URL);
 module.exports = redisClient;
